75104cf1bf78709afddd0934e465b6946946dff2
Aryog <yogesh.772417@thc.tu.edu.np>
2023-05-12 17:07:31 +0545

    home screen setup

--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fundflow/utils/app_layout.dart';
 import 'package:fundflow/utils/app_styles.dart';
+import 'package:gap/gap.dart';
 
 class AppHomeScreen extends StatelessWidget {
   const AppHomeScreen({super.key});
@@ -7,11 +9,82 @@ class AppHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          child: Text(
-        "Hello World",
-        style: Styles.headLineStyle1,
-      )),
-    );
+        backgroundColor: Styles.bgColor,
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: AppLayout.getHeight(240),
+                    decoration: const BoxDecoration(
+                      color: Color(0xff368983),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20)),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          top: 35,
+                          left: 340,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(7),
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              color: Color.fromRGBO(250, 250, 250, 0.1),
+                              child: const Icon(
+                                Icons.notification_add_outlined,
+                                size: 30,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 30, left: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Good Morning!",
+                                  style: Styles.headLineStyle1
+                                      .copyWith(color: Colors.grey.shade300)),
+                              const Gap(5),
+                              Text(
+                                "Yogesh Aryal",
+                                style: Styles.headLineStyle2
+                                    .copyWith(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: 160,
+                left: 37,
+                child: Container(
+                  height: 170,
+                  width: 320,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 47, 125, 121),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [Text("Total Balance")],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/pages/LoginScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AppLoginScreen extends StatelessWidget {
+  const AppLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Hello Login"),
+      ),
+    );
+  }
+}
